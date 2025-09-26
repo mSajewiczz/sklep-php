4c6dee46ea0b36cebbae073c28d7d1571bd8f119
Mikołaj Sajewicz <107215730+mSajewiczz@users.noreply.github.com>
2025-09-26 12:43:29 +0200

    Quick fix

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php 
+
+
+$server = "localhost";
+$dbname = "sklep_db";
+$connection = mysqli_connect($server, "root", "", $dbname);
+
+$sql = "SELECT * FROM products";
+
+$result = mysqli_query($connection, $sql);
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="pl">
 
@@ -117,6 +131,33 @@
         <div class="products-header">
             <h2>Produkty</h2>
             <p>Sprawdź naszą bogatą ofertę produktów!</p>
+        </div>
+
+        <div class="products-list">
+
+            <?php 
+            
+                while($row = mysqli_fetch_assoc($result))
+                {
+                    ?>
+
+            <div class="product">
+                <img class="product-img" src="" alt="Photo of product.">
+                <h4 class="product-name"></h4>
+                <p class="product-desc"></p>
+                <p class="product-price"></p>
+                <p class="product-rating"></p>
+
+
+
+            </div>
+
+
+            <?php>
+            }
+
+            ?>
+
         </div>
 
     </section>
